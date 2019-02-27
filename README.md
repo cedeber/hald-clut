65938f3b76b8886978ad881ca986217ccce7c294
Cédric Eberhardt <hello+code@cedeber.fr>
2019-02-27 23:08:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HaldCLUT
+A collection of HaldCLUT
+
+- [RawTherapee Film Simulation](http://rawpedia.rawtherapee.com/Film_Simulation)
+- [PictureFX v2](http://www.digicrea.be/picturefx/)
---
+++ convert.sh
@@ -0,0 +1,15 @@
+# Needs ImageMagick + OptiPNG
+# Convert original HaldCLUT to smaller HardCLUT in order to use them in web applications.
+
+IFS=$'\n'
+for file in $(find HaldClut -name \*.png -type f); do
+    dir=$(dirname $(echo $file | cut -d'/' -f2-))
+    filename=$(basename $file)
+    out=$(echo "out/$dir/$filename" | tr " " "_" | tr "[:upper:]" "[:lower:]")
+
+    echo "$dir : $filename\\n=> $out\\n"
+
+    mkdir -p $(dirname $out)
+    convert hald:8 -depth 8 -colorspace sRGB $file -hald-clut $out
+    optipng -o1 -silent $out
+done
