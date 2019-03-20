d663dd38b80ddce9f53572cbb5e717e1471ede28
Cédric Eberhardt <hello+code@cedeber.fr>
2019-03-20 19:47:08 +0100

    Add how to make an identity

--- convert.sh
+++ convert.sh
@@ -1,6 +1,9 @@
 # Needs ImageMagick + OptiPNG
-# Convert original HaldCLUT to smaller HaldCLUT in order to use them in web applications.
 
+# Generate an identity 12-level 16-bit HaldCLUT image using ImageMagick
+# convert hald:12 -depth 16 -colorspace sRGB hald12_16bit.tif
+
+# Convert original HaldCLUT to smaller HaldCLUT in order to use them in web applications.
 IFS=$'\n'
 for file in $(find HaldClut -name \*.png -type f); do
     dir=$(dirname $(echo $file | cut -d'/' -f2-))
